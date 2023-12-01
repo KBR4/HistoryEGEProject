6272d9be3f123f8c7e8f8f47b03ec0b47e8ea473
EgorSamoylov <mr.egoras@mail.ru>
2023-12-01 11:39:16 +0300

    Did TO DO in Test.cs using regular expression and function File.Exists(). Check this in Console app.

--- HistProjTemplate/HistLib/Test.cs
+++ HistProjTemplate/HistLib/Test.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace HistLib
@@ -22,12 +24,56 @@ namespace HistLib
         public Test(String name, string source, List<QuestionAnswer> QA)    //Создание темы по названию, изображению, массиву вопросов-ответов
         {
             Name = name;
-            Source = source;            //TO DO: проверка подгрузки картинки
+            Source = source;            //TO DO: проверка подгрузки картинки - CheckUploadImage()
             AllQuestionsAnswers = QA;
         }
         public override string ToString()
         {
             return Name;
+        }
+
+        public static bool CheckUploadImage(string source)
+        {
+            if (!File.Exists(source) && CheckImageExpansion(source))
+            {
+                Console.WriteLine("Ты Молодец!!!");
+                return true;
+            }
+            else
+            {
+                if (!File.Exists(source))
+                {
+                    //Файл не существует
+                    return false;
+                }
+                else
+                {
+                    if (CheckImageExpansion(source))
+                    {
+                        //Ошибка в ссылке
+                        return false;
+                    }
+                    else
+                    {
+                        //Егор, ты косяк!
+                        return false;
+                    }
+                }
+            }
+        }
+        // найдем в пути, является ли файл .png, .jpg и тд 
+        public static bool CheckImageExpansion(string source)
+        {
+            Regex regex = new Regex(@"[^\s]+(?=\.(jpg|gif|png|jpeg))\.");
+            Match match = regex.Match(source);
+            if (match.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public Statistics GetResults(string[] UserAnswers)
